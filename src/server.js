be746878748d01d1fc514988f97196c0404d7398
Justin Hammonds <justinrhammonds@gmail.com>
2019-08-25 14:38:03 -0400

    added cors whitelist

--- src/server.js
+++ src/server.js
@@ -6,14 +6,12 @@ import resolvers from "./graphql/resolvers";
 import "./db/dbConnection";
 
 const server = new ApolloServer({
+  cors: {
+    origin: process.env.CORS_WHITELIST,
+    credentials: true
+  },
   typeDefs: schema,
   resolvers
-  // TODO
-  // context: ({ req }) => {
-  //   // get the auth token from the headers
-  //   const token = req.headers.authentication || null;
-  //   if (!token) console.error("you must be logged in");
-  // }
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
